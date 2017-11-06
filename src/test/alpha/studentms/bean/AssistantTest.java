4dbbe52b44735b6aa0747dc584b79a84974cc0b4
DcoOo <18510786796@163.com>
2017-11-06 16:28:03 +0800

    创建web.xml文件

---
+++ src/test/alpha/studentms/bean/AssistantTest.java
@@ -0,0 +1,14 @@
+package test.alpha.studentms.bean;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssistantTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
