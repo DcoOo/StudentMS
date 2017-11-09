46b76aed8fdbab8e83d1dadc0ebd607e12b77223
SunBoy <18813080818@163.com>
2017-11-09 18:44:00 +0800

    增加学生选做和必做备忘录DAO层

---
+++ src/test/alpha/studentms/dao/StableMemoDAOTest.java
@@ -0,0 +1,56 @@
+package test.alpha.studentms.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import alpha.studentms.bean.Memo;
+import alpha.studentms.dao.StableMemoDAO;
+
+/**
+ * StableMemoDAO的测试类
+ * 
+ * @author joker
+ *
+ */
+public class StableMemoDAOTest {
+
+	StableMemoDAO stableMemoDAO;
+
+	@Before
+	public void init() {
+		stableMemoDAO = new StableMemoDAO();
+	}
+
+	@Test
+	public void testInsertStableMemo() {
+		stableMemoDAO.insertStableMemo("2", "1", "2");
+	}
+
+	@Test
+	public void testDeleteStableMemoById() {
+		stableMemoDAO.deleteStableMemoById("2", "1");
+	}
+
+	@Test
+	public void testGetAllCompulsoryMemo() {
+		List<Memo> list = new ArrayList<>();
+		list = stableMemoDAO.getAllCompulsoryMemo("2");
+		System.out.println(list);
+	}
+
+	@Test
+	public void testGetAllOptionalMemo() {
+		List<Memo> list = new ArrayList<>();
+		list = stableMemoDAO.getAllOptionalMemo("2");
+		System.out.println(list);
+	}
+
+	@After
+	public void close() {
+		stableMemoDAO.close();
+	}
+}
