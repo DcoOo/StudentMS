5ab8d2b178a4307b3107389a59a8813e9ca25693
DcoOo <18510786796@163.com>
2017-11-13 16:16:10 +0800

    添加JSTL包

--- src/alpha/studentms/controller/DeleteMemoController.java
+++ src/alpha/studentms/controller/DeleteMemoController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.connector.Request;
+
 import alpha.studentms.service.MemoService;
 import alpha.studentms.service.StudentService;
 import alpha.studentms.serviceImple.MemoServiceImple;
@@ -37,8 +39,17 @@ public class DeleteMemoController extends HttpServlet{
 		// 获取待删除Memo的id
 		String memo_id = (String)req.getParameter("id");
 		// Memo类型
-		String type = (String)req.getParameter("type");
-//		memoService.deleteMemoByMemoId(memo_id, user_id, MemoService.);
+		int type = Integer.parseInt(req.getParameter("type"));
+		switch (type) {
+		case 0:
+			// 0表示删除选做任务和必做任务
+			memoService.deleteMemoByMemoId(memo_id, user_id, MemoService.MUST_AND_OPTION_MEMO_CODE);
+			break;
+		case 1:
+			// 1表示删除自定义任务
+			break;
+		}
+		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 
 	}
 
